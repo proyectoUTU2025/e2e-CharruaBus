3d4e8a35eeadec4f81e7f371a8ce6786dd006600
bentanic-0 <bentanico@gmail.com>
2025-07-15 09:36:55 -0300

    Update Playwright test case for valid login credentials with new test data

--- playwright-tests/Inicio de sesión/TestCase1.spec.ts
+++ playwright-tests/Inicio de sesión/TestCase1.spec.ts
@@ -8,23 +8,23 @@ test('Test Case 1: Login con credenciales válidas', async ({ page }) => {
 
   //3.Verificar que se muestre el boton de login
   await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
-  
+
   //4.Presiona el boton de login
   await page.getByRole('link', { name: 'Login' }).click();
 
   //5.Verificar que se muestre el formulario de login con campos "Email" y "Contraseña"
   await expect(page.getByRole('textbox', { name: 'Email' })).toBeVisible();
   await expect(page.getByRole('textbox', { name: 'Contraseña' })).toBeVisible();
- 
+
   //6.Ingresar un email y contraseña válidos
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('gameftjp@gmail.com');
+  await page.getByRole('textbox', { name: 'Email' }).fill('admin@test.com');
   await page.getByText('Contraseña', { exact: true }).click();
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Pepito11!');
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Admin123!$');
 
   //7.Pulsar el botón "Iniciar sesión"
   await page.getByRole('button', { name: 'Iniciar sesión' }).click();
 
   //8.Verificar que el usuario sea redirigido a la pantalla principal según su rol
-  await expect(page.locator('#mat-mdc-chip-0').getByText('CLIENTE')).toBeVisible();
+  await expect(page.locator('#mat-mdc-chip-0').getByText('ADMIN')).toBeVisible();
 });
