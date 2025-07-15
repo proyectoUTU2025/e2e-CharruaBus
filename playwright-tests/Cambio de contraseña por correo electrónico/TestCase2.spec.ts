a39cbefb3b736c565e6b69a6e8c2c0a03a765aa1
bentanic-0 <bentanico@gmail.com>
2025-07-15 09:36:27 -0300

    Add Playwright test cases for password recovery scenarios

---
+++ playwright-tests/Cambio de contraseña por correo electrónico/TestCase2.spec.ts
@@ -0,0 +1,28 @@
+// import { test, expect } from '@playwright/test';
+
+// test('Test Case 2: Correo no registrado', async ({ page }) => {
+
+//   //1.Lanzar el navegador
+//   //2.Navegar a la URL de la aplicación 
+//   await page.goto('http://localhost:4200/');
+
+//   //3.Verificar que se muestre el boton de login
+//   await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+
+//   //4.Ir a la página de login
+//   await page.getByRole('link', { name: 'Login' }).click();
+
+//   //5.Hacer clic en “¿Olvidó su contraseña?”
+//   await expect(page.getByText('¿Olvidaste tu contraseña?')).toBeVisible();
+//   await page.getByText('¿Olvidaste tu contraseña?').click();
+
+//   //6.Ingresar un correo no registrado
+//   await page.getByRole('textbox', { name: 'Email' }).click();
+//   await page.getByRole('textbox', { name: 'Email' }).fill('correo1@gmail.com');
+ 
+//   //7.Pulsar “Enviar código”
+//   await page.getByRole('button', { name: 'Enviar código' }).click();
+
+//   //8.Verificar que se muestre un mensaje de error
+//   await expect(page.getByText('El correo ingresado no existe')).toBeVisible();
+// });
---
+++ playwright-tests/Cambio de contraseña por correo electrónico/TestCase4.spec.ts
@@ -0,0 +1,41 @@
+// import { test, expect} from '@playwright/test';
+// import { promptUser } from "../../utils/prompt"
+
+// test('Test Case 4: Nueva contraseña igual a la anterior', async ({ page }) => {
+
+//   //1.Lanzar el navegador
+//   //2.Navegar a la URL de la aplicación  
+//   await page.goto('http://localhost:4200/');
+
+//   //3.Ir a la página de login
+//   await page.getByRole('link', { name: 'Login' }).click();
+
+//   //4.Hacer clic en “¿Olvidó su contraseña?”
+//   await page.getByText('¿Olvidaste tu contraseña?').click();
+
+//   //5.Ingresar un correo válido y registrado
+//   await page.getByRole('textbox', { name: 'Email' }).click();
+//   await page.getByRole('textbox', { name: 'Email' }).fill('gameftjp@gmail.com');
+
+//   //6.Pulsar “Enviar código”
+//   await page.getByRole('button', { name: 'Enviar código' }).click();
+
+//   //7.Ingresar el codigo de verificación
+//   await page.pause()
+
+//   //8.Presionar "Verificar Código" para verificar que el codigo sea valido
+//   await page.getByRole('button', { name: 'Verificar Código' }).click();
+//   await page.waitForLoadState("networkidle")
+
+//   //9.Ingresar la misma contraseña anterior como nueva
+//   await page.getByText('Nueva Contraseña', { exact: true }).click();
+//   await page.getByRole('textbox', { name: 'Nueva Contraseña' }).fill('Pepito12!');
+//   await page.getByText('Confirmar Contraseña').click();
+//   await page.getByRole('textbox', { name: 'Confirmar Contraseña' }).fill('Pepito12!');
+
+//   //10.Presiona "Cambiar Contraseña"
+//   await page.getByRole('button', { name: 'Cambiar Contraseña' }).click();
+
+//   //11.Verificar que se muestra el mensaje: “La nueva contraseña debe ser distinta a la anterior”
+//   await expect(page.getByText('La nueva contraseña debe ser')).toBeVisible();
+// });
